4c25301092e0a29039d270d2130c8aa59636648a
Pooya Taherkhani <pt376511@ohio.edu>
2019-04-06 11:35:02 -0400

    Created first script--check number of positional arguments

---
+++ src/purchase_analytics.py
@@ -0,0 +1,24 @@
+
+# For each row in order-products table:
+#     Read the product-id and `reordered` value
+#     In products table, find the department-id associated with the product-id
+#     In report table, find the row associated with the department-id
+#         If department-id doesn't exist, create a row for it
+#         Increment number-of-orders
+#         If `reordered` is 0
+#             Increment number-of-first-orders
+# Compute ratio for each row in report table
+# Sort report table based on department-id
+
+
+from optparse import OptionParser
+
+parser = OptionParser()
+(options, args) = parser.parse_args()
+if len(args) != 3:
+    raise ValueError('At least one of three required input arguments is\
+ missing.')
+
+# print('first argument:', args[0])
+
+# # open order_products.csv
