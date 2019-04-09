3715d405aab5bff960b24bd42e24318b4978e165
Pooya Taherkhani <pt376511@ohio.edu>
2019-04-08 21:38:27 -0400

    First working version of the program, yay!

--- src/generate_report.py
+++ src/generate_report.py
@@ -1,30 +1,57 @@
-# For each row in order-products table:
-#     Read the product-id and `reordered` value
-#     In products table, find the department-id associated with the product-id
-#     In report table, find the row associated with the department-id
-#         If department-id doesn't exist, create a row for it
-#         Increment number-of-orders
+# For each row in order_products table:
+#     Read the prod_id and `reordered` value
+#     In products table, find the dept_id associated with the prod_id
+#     In report table, find the row associated with the dept_id
+#         If dept_id exists, just increment number_of_orders
+#         Otherwise, create a row for it and set number_of_orders to 1
 #         If `reordered` is 0
-#             Increment number-of-first-orders
+#             Increment number_of_first_orders
 # Compute ratio for each row in report table
-# Sort report table based on department-id
+# Sort report table based on dept_id
 
 
 from optparse import OptionParser
 import products
 import checks
+import csv
 
 parser = OptionParser()
 (options, args) = parser.parse_args()
 checks.check_num_args(args)
-print(args)
 order_prod_filename, prod_filename, report_filename = args
 
 # load products table
 prod_table = products.load_prod_table(prod_filename)
 
+report_table = {}
 
-print("__name__ == '__main__' is", __name__ == '__main__')
+with open(order_prod_filename, newline='') as ord_file:
+    ord_file = products.remove_header(ord_file)
+    ord_table = csv.reader(ord_file)
+    for row in ord_table:
+        prod_id = row[1]
+        reordered = row[3]
+        dept_id = int(prod_table[prod_id])
+        # Two lookups in the dictionary for each key
+        if dept_id in report_table:  # first lookup for all cases
+            dept_spec = report_table[dept_id]  # second lookup
+            dept_spec[0] += 1
+        else:
+            dept_spec = [1, 0, 0.0]
+            report_table[dept_id] = dept_spec  # second lookup
+        if reordered == '0':
+            dept_spec[1] += 1
+
+with open(report_filename, 'w', newline='') as csv_report:
+    report = csv.writer(csv_report)
+    for key in sorted(report_table):
+        dept_row = report_table[key]
+        num_orders, num_first_orders = dept_row[0], dept_row[1]
+        dept_row[2] = num_first_orders / num_orders
+        report.writerow([key] + dept_row)
+
+print(sorted(report_table))
+
 print("Done!")
 
 # print('first argument:', args[0])
